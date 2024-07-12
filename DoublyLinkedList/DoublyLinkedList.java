0142e663a3ccb2ba79f67fe5ebde82d47c5adee6
Horidas Roy <royhoridas64@gmail.com>
2024-07-12 10:56:48 +0600

    created displayForward and displayBackward method

---
+++ DoublyLinkedList/DoublyLinkedList.java
@@ -0,0 +1,64 @@
+package DoublyLinkedList;
+
+public class DoublyLinkedList {
+     private ListNode head;
+     private ListNode tail;
+     private int length;
+     
+     private class ListNode {
+        private int data;
+        private ListNode next;
+        private ListNode previous;
+
+        public ListNode(int data){
+            this.data = data;
+        }
+     }
+
+     public DoublyLinkedList(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+     }
+
+     public boolean isEmpty(){
+        return length == 0; ///head == null
+     }
+
+     public int length(){
+        return length;
+     }
+
+//print element of DoublyLinkedList in forward direction
+public void displayForward(){
+     if(head == null){
+      return;
+     }
+     ListNode temp = head;
+     while (temp != null) {
+         System.out.println(temp.data+"-->");
+         temp = temp.next;
+     }
+     System.out.println("null");
+}
+
+//print element of DoublyLinkedList in backward direction
+public void displayBacward(){
+   if(head == null){
+      return;
+   }
+   ListNode temp = tail;
+   while (temp != null) {
+      System.out.println(temp.data+"-->");
+      temp = temp.previous;
+   }
+   System.out.println("null");
+}
+
+
+     public static void main(String[] args) {
+            DoublyLinkedList dll = new DoublyLinkedList();
+
+            
+     }
+}
