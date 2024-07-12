f10f371dc2e7ee70f8b80c0c9e15b4876a97f1de
Horidas Roy <royhoridas64@gmail.com>
2024-07-12 22:01:14 +0600

    implement Queue and added Enqueue and Dequeue method.also added print method

---
+++ Queue.java
@@ -0,0 +1,84 @@
+import java.util.NoSuchElementException;
+
+public class Queue {
+    private ListNode front;
+    private ListNode rear;
+    private int length;
+
+    private static class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data){
+              this.data = data;
+              this.next = null;
+        }
+        
+    }
+    public Queue(){
+         this.front = null;
+         this.rear = null;
+         this.length = 0;
+    }
+    public int length(){
+        return length;
+    }
+    public boolean isEmpty(){
+        return length == 0;
+    }
+
+    //print a queue
+    public void print(){
+        if (isEmpty()) {
+            return;
+        }
+        ListNode current = front;
+        while (current != null) {
+            System.out.print(current.data+"-->");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    //ENQUEUE data
+    public void enQueue(int data){
+        ListNode temp = new ListNode(data);
+        if(isEmpty()){
+            front = temp;
+            // rear = temp;
+        }else{
+            rear.next = temp;
+            // rear = temp;
+        }
+        rear = temp;
+        length++;
+    }
+
+//DEQUEUE 
+public int deQueue(){
+    if (isEmpty()) {
+        throw new NoSuchElementException();
+    }
+    int result = front.data;
+    front = front.next;
+    if(front == null){
+        rear = null;
+    }
+    length--;
+    return result;
+}
+
+    public static void main(String[] args) {
+          Queue queue = new Queue();
+
+          queue.enQueue(1);
+          queue.enQueue(5);
+          queue.enQueue(10);
+          queue.enQueue(15);
+          queue.print();
+          queue.deQueue();
+          queue.deQueue();
+          queue.deQueue();
+          queue.deQueue();
+          queue.print();
+    }
+}
