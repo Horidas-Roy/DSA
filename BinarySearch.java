b5201049e1f7579745f41985ec7b83ca049d8a59
Horidas Roy <royhoridas64@gmail.com>
2024-07-15 11:43:13 +0600

    Binary Search

---
+++ BinarySearch.java
@@ -0,0 +1,26 @@
+
+
+public class BinarySearch {
+    public int search(int[] arr, int key){
+          int low = 0;
+          int high = arr.length-1;
+          while (low <= high) {
+             int mid = (low+high)/2;
+             if (arr[mid] == key) {
+                return mid;
+             }
+             if (key < arr[mid]) {
+                high = mid -1;
+             }else{
+                low = mid+1;
+             }
+          }
+          return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {4,5,3,6,8,1,9,0,3,2};
+        BinarySearch bs = new BinarySearch();
+        int result = bs.search(arr, 10);
+        System.out.println("Search key at index: "+ result);
+    }
+}
--- Search/LinearSearch.java
+++ Search/LinearSearch.java
@@ -1,3 +1,4 @@
+package Search;
 
 
 public class LinearSearch {
